edf9b926d4542537dbc8d97b31806200fb011ef9
Johnldelaumbria <35165772+delaumbriajohn@users.noreply.github.com>
2021-03-16 09:17:26 +0800

    ATM Simulation app v1
    This version i created the login and register form. further improvement and features will added on future updates.

---
+++ ATM_Simulation/CreateAccountWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ATM_Simulation
+{
+    /// <summary>
+    /// Interaction logic for CreateAccountWindow.xaml
+    /// </summary>
+    public partial class CreateAccountWindow : Window
+    {
+        public CreateAccountWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BacktoLoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            this.Close();
+        }
+    }
+}
